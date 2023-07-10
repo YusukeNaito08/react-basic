a35806c90d2db41c46e19785f987c81d62e087d9
YusukeNaito08 <ys.training210625@gmail.com>
2023-07-10 23:57:01 +0900

    react-todo_practice1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {TodoList} from "./TodoList";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          React チュートリアル
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [todos, setTodos] = useState(["todo1", "todo2"])
+
+return (
+  <>
+    <TodoList todos = {todos}/>
+    <input type="text"/>
+    <button>タスクを追加</button>
+    <button>完了したタスクを追加</button>
+    <div>残りのタスクを追加：0</div>
+  </>
+)
 }
 
 export default App;
