bcf1840e36bae6d50349e8ba7c360a52e3ba0f6b
YusukeNaito08 <yusuke0208na@gmail.com>
2023-07-14 09:24:35 +0900

    react-todo_practice3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import {TodoList} from "./TodoList";
+import {v4 as uuidv4} from "uuid"
 
 
 function App() {
@@ -8,11 +9,31 @@ function App() {
     {id: 1, name: "Todo1", completed: false},
   ])
 
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    //タスクを追加
+    const name = todoNameRef.current.value
+    setTodos((prevTodos) => {
+      return [... prevTodos, {id: uuidv4(), name: name, completed: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed
+    setTodos(newTodos)
+
+  }
+
 return (
   <>
-    <TodoList todos = {todos}/>
-    <input type="text"/>
-    <button>タスクを追加</button>
+    <TodoList todos = {todos} toggleTodo = {toggleTodo}/>
+    <input type="text" ref= {todoNameRef}/>
+    <button onClick = {handleAddTodo}>タスクを追加</button>
     <button>完了したタスクを追加</button>
     <div>残りのタスクを追加：0</div>
   </>
